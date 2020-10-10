4234c6483b2015302ca189303378635370b85138
Austin Law <akl29@njit.edu>
2020-10-09 20:30:08 -0400

    Fixed and updated front-end to match milestone 3

--- front-end/app.py
+++ front-end/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
+def index():
+	return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
