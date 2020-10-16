70db04a2015612d890dc9bba5606a493a42e7cca
Brandon Tran <brandontran@Bs-MacBook-Air.local>
2020-10-16 16:00:51 -0400

    Added password field to user table; In /db/aCode.sql

--- db/aCode.sql
+++ db/aCode.sql
@@ -1,4 +1,7 @@
-CREATE TABLE user (name VARCHAR(20));
+CREATE TABLE user (
+name VARCHAR(20)
+password VARCHAR(255)
+);
 INSERT INTO user VALUES ("Alice");
 INSERT INTO user VALUES ("Bob");
 INSERT INTO user VALUES ("Carol");
