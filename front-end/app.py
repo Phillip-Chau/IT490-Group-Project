4b259186bc4c5fd2593da5c0376b8cdc485388f6
Austin Law <akl29@njit.edu>
2020-10-17 16:24:57 -0400

    Made changes to front-end to match midterm deliverables

--- front-end/app.py
+++ front-end/app.py
@@ -1,13 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
+from functools import wraps
+from werkzeug.security import check_password_hash, generate_password_hash
+import logging
+import messaging
+import os
+
 app = Flask(__name__)
+app.secret_key = os.enviorn['FLASK_SECRET_KEY']
+
+logging.basicConfig(level=logging.INFO)
+
+def login_required(f):
+
+    @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if 'uname' not in session:
+                return redirect('/login')
+            return f(*args, **kwargs)
+        return decorated_function
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/register')
+@app.route('/secret')
+@login_required
+def secret():
+    return render_template('secret.html')
+    
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        uname = request.form['uname']
+        password = request.form['password']
+        msg = messaging.Messaging()
+        msg.send(
+            'REGISTER',
+            {
+                'uname': uname,
+                'hash': generate_password_hash(password)
+            }
+        )
+        response = msg.receive()
+        if response['success']:
+            session['uname'] = uname
+            return redirect('/')
+        else:
+            return f"{response['message']}"
     return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        uname = request.form['uname']
+        password = request.form['password']
+        msg = messaging.Messaging()
+        msg.send('GETHASH', { 'uname': uname })
+        response = msg.receive()
+        if response['success'] != True:
+            return "Login failed."
+        if check_password_hash(response['hash'], password):
+            session['uname'] = uname
+            return redirect('/')
+        else:
+            return "Login failed."
+    return render_template('login.html')
+    
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect('/')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
